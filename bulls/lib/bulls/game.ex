6d688a0b8242e99c5e2869683075984b41f3f4e8
Liam Brown <liamnb525@gmail.com>
2021-02-18 16:27:07 -0500

    Enum.random docs are DEAD WRONG

--- bulls/lib/bulls/game.ex
+++ bulls/lib/bulls/game.ex
@@ -34,7 +34,7 @@ defmodule Bulls.Game do
       Enum.join(digits, "")
     end
 
-    digit = Enum.random([1,2,3,4,5,6,7,8,9])
+    digit = Enum.random([1,2,3,4,5,6,7,8,9])[0]
     IO.puts("Trying #{digit}")
     if !MapSet.member?(MapSet.new(digits), digit) do
       IO.puts("Retry")

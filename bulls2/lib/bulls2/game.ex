1a31ff2e5e025c2f93d0ddc4e1ff353477cd7913
Liam Brown <liamnb525@gmail.com>
2021-02-26 18:26:44 -0500

    f

--- bulls2/lib/bulls2/game.ex
+++ bulls2/lib/bulls2/game.ex
@@ -151,7 +151,9 @@ defmodule Bulls2.Game do
           # Prepare next round for play, setting the autopass timer
           IO.puts("Moving to next round")
           IO.inspect(updated_rounds)
+          IO.inspect(Enum.count(updated_rounds))
           IO.inspect(List.insert_at(updated_rounds, 1000000, %{}))
+          IO.inspect(Enum.count(List.insert_at(updated_rounds, 1000000, %{})))
            {
             true, "",
             %{st | rounds: List.insert_at(updated_rounds, Enum.count(updated_rounds), %{})},

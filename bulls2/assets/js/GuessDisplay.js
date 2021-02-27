5300fedb7f0d3b2acd39a92ad30bd40cf90b2e79
Liam Brown <liamnb525@gmail.com>
2021-02-26 19:39:12 -0500

    f

--- bulls2/assets/js/GuessDisplay.js
+++ bulls2/assets/js/GuessDisplay.js
@@ -1,29 +1,32 @@
 import "../css/GuessDisplay.css";
 
-function makeRow(playerAndGuess, round){
-  return(
+function makeRow(playerAndGuess, round) {
+  return (
     <div className="GuessDisplay-row" key={playerAndGuess[0] + round}>
-      <div>{playerAndGuess[0]}: </div>
+      <div className="GuessDisplay-playerlabel">{playerAndGuess[0]}: </div>
       <div>{playerAndGuess[1].guess}</div>
       <div>Bulls: {playerAndGuess[1].bulls}</div>
       <div>Cows: {playerAndGuess[1].cows}</div>
     </div>
-  )
+  );
 }
 
-
 function GuessDisplay({ state: { rounds } }) {
+  let reversed_rounds = rounds.reverse();
   return (
     <div className="GuessDisplay">
       <div className="GuessDisplay-list">
-        {rounds.map((players, i) => (
-          <div className="GuessDisplay-element" key={i}> Round {i + 1}: {Object.entries(players).map( function(x) { return makeRow(x, i); })}
+        {reversed_rounds.map((players, i) => (
+          <div className="GuessDisplay-element" key={i}>
+            <span className="GuessDisplay-label">Round {i + 1}: </span>
+            {Object.entries(players).map(function (x) {
+              return makeRow(x, i);
+            })}
           </div>
         ))}
-     </div>
-   </div>   
-  )
+      </div>
+    </div>
+  );
 }
-
 
 export default GuessDisplay;
